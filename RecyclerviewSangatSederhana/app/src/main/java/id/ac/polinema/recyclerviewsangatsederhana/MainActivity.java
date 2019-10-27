b2f22d0626deff799c73f921e4e19ed58a805525
arip1997 <ariefr.santoso@gmail.com>
2019-10-28 02:31:07 +0700

    Project No.3

--- RecyclerviewSangatSederhana/app/src/main/java/id/ac/polinema/recyclerviewsangatsederhana/MainActivity.java
+++ RecyclerviewSangatSederhana/app/src/main/java/id/ac/polinema/recyclerviewsangatsederhana/MainActivity.java
@@ -1,41 +1,36 @@
 package id.ac.polinema.recyclerviewsangatsederhana;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import id.ac.polinema.recyclerviewsangatsederhana.adapters.SuperHeroAdapter;
+import id.ac.polinema.recyclerviewsangatsederhana.adapter.DummyHeroAdapter;
+import id.ac.polinema.recyclerviewsangatsederhana.adapter.SuperHeroAdapter;
+import id.ac.polinema.recyclerviewsangatsederhana.models.DummyHero;
 import id.ac.polinema.recyclerviewsangatsederhana.models.SuperHero;
 
 public class MainActivity extends AppCompatActivity {
-    //instansiasi Recyclerview
     RecyclerView rvSuperHero;
-    //instansiasi list superhero
-    List<SuperHero> listSuperHero = new ArrayList<>();
+    List<DummyHero> listSuperHero = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //menyambungkan rvSuperHero ke layout
         rvSuperHero = findViewById(R.id.rvSuperHero);
-        //Membuat object hero
-        SuperHero hero = new SuperHero("Petruk");
-        //menambahkan hero ke listSuperHero
+        DummyHero hero = new DummyHero("Instagram", "https://img.icons8.com/cotton/2x/instagram-new.png");
         listSuperHero.add(hero);
-        //membuat object hero baru
-        hero = new SuperHero("Gareng");
-        //menambahkan hero baru ke listSuperhero
+        hero = new DummyHero("Twitter", "https://img.icons8.com/plasticine/2x/twitter.png");
         listSuperHero.add(hero);
-        //Instansiasi Adapter
-        SuperHeroAdapter superHeroAdapter = new SuperHeroAdapter(listSuperHero);
-        //set adapter dan layoutmanager
-        rvSuperHero.setAdapter(superHeroAdapter);
-        rvSuperHero.setLayoutManager(new LinearLayoutManager(this));
+        DummyHeroAdapter dummyHeroAdapter = new DummyHeroAdapter(listSuperHero);
+        rvSuperHero.setAdapter(dummyHeroAdapter);
+        rvSuperHero.setLayoutManager(new GridLayoutManager(this, 2));
     }
 }
